0c3224ed7281965b34b2cc6d649027d6b0c8faf1
Patryk Szmurło <patryk.szmurlo@intive.com>
2025-04-25 14:49:35 +0200

    feat: adjust spacing, and demoBanner

--- components/es2025/aop/demoBanner/demoBanner.tsx
+++ components/es2025/aop/demoBanner/demoBanner.tsx
@@ -1,14 +1,31 @@
-import { Headline, Typography } from "@vorwerk/fibre-react"
+import {
+  GridContainer,
+  GridItem,
+  Headline,
+  Typography,
+} from "@vorwerk/fibre-react";
 import styles from "./demoBanner.module.scss";
 
 export const DemoBanner = () => {
   return (
-    <div className={styles.bannerWrapper} style={{
-      background: "linear-gradient(180deg, rgba(0, 0, 0, 0.12) 0%, rgba(0, 0, 0, 0.32) 59.62%, rgba(0, 0, 0, 0.12) 100%), url('/homePage/aop/category_tile.png')",
-    }}>
-      <Headline strongColor="white">
-        <Typography variant="h1"><strong>Try</strong> before buy</Typography>
-      </Headline>
+    <div
+      className={styles.bannerWrapper}
+      style={{
+        background:
+          "linear-gradient(180deg, rgba(0, 0, 0, 0.12) 0%, rgba(0, 0, 0, 0.32) 59.62%, rgba(0, 0, 0, 0.12) 100%), url('/homePage/aop/category_tile.png')",
+        backgroundSize: "cover",
+      }}
+    >
+      <GridContainer>
+        <GridItem columns={12}>
+          <Headline strongColor="white">
+            <Typography variant="h1">
+              <strong>Try</strong> before buy
+            </Typography>
+          </Headline>
+          <div data-scroll-marker="aop-sticky-start" />
+        </GridItem>
+      </GridContainer>
     </div>
-  )
-}
+  );
+};
